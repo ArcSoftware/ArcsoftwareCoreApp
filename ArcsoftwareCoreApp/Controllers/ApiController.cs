688ec5ec5e54fa5683644146c4bbe98d21e39e3e
Jake <dev.jaket@gmail.com>
2018-06-04 16:07:31 -0400

    adding api controller and sagger ui

---
+++ ArcsoftwareCoreApp/Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArcsoftwareCoreApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/")]
+    public class ApiController : Controller
+    {
+        [HttpPost("{id:int}")]
+        public int ShowProduct(int id)
+        {
+            return id;
+        }
+    }
+}
--- ArcsoftwareCoreApp/Startup.cs
+++ ArcsoftwareCoreApp/Startup.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ArcsoftwareCoreApp
 {
@@ -22,6 +19,10 @@ namespace ArcsoftwareCoreApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +32,11 @@ namespace ArcsoftwareCoreApp
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
             }
             else
             {
